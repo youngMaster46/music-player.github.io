401e560ed3d883f3b37407b550475f96f0b29146
youngMaster46 <kaplin95@inbox.ru>
2020-06-19 05:36:05 +0300

    add ending for songs

--- script.js
+++ script.js
@@ -106,3 +106,6 @@ audio.addEventListener('timeupdate', updateProgress)
 
 // Click on progress bar 
 progressContainer.addEventListener('click', setProgress)
+
+// Song ends
+audio.addEventListener('ended', nextSong)
